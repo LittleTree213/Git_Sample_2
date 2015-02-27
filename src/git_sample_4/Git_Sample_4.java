829ea8361f0b84b22c5d1b72e560fe9956374413
Nahal <Nahal@LittleTree-pc>
2015-02-27 07:21:07 -0800

    Repository Name_Git_Sample_4_2

--- src/git_sample_4/Git_Sample_4.java
+++ src/git_sample_4/Git_Sample_4.java
@@ -15,7 +15,7 @@ public class Git_Sample_4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("One of the configuration items changed!");
     }
     
 }
